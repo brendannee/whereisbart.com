5c71f1186a8d9fd81708de0357912e02ffca9862
Aviv Bueno <aviv.buen@gmail.com>
2019-05-02 10:31:48 +0300

    Adding a simple filtering option and removing the sidebar
    Remving sidebar and adding bart-line-destination filter

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 //ref: https://leafletjs.com/reference-1.3.4.html#marker
 
 // constants
-var BART_API_URI = 'https://api.bart.gov/api/';
-var BART_API_KEY = 'MW9S-E7SL-26DU-VV8V';
+var BART_API_URI = "https://api.bart.gov/api/";
+var BART_API_KEY = "MW9S-E7SL-26DU-VV8V";
 var REFRESH_FREQ = 5000; // in millis
 var STATION_OPACITY = 0.6;
 
@@ -16,10 +16,11 @@ var lastProcTime;
 var liveTrains = [];
 var showingStation;
 var activeMarker;
+var selectedTrain = "all";
 
 // debugging
 var debugMode = false;
-var debugText = '';
+var debugText = "";
 
 /*----------------------------------------------------------------------*\
     Setup
@@ -40,21 +41,35 @@ function buildTimes() {
 }
 
 /*----------------------------------------------------------------------*\
-    Bart Station
+    Bart Train Selection Set
 \*----------------------------------------------------------------------*/
+function setSelectedTrain(destination) {
+  selectedTrain = destination;
+}
+/*----------------------------------------------------------------------*\
+    Bart Station
+\*-----------------------------------------------------------------z-----*/
 function getBartStation(stationKey, marker) {
   if (showingStation == stationKey) {
-    showingStation = '';
+    showingStation = "";
     activeMarker = undefined;
     return;
   }
-  $.get(BART_API_URI + 'etd.aspx?cmd=etd&orig=' + stationKey + '&key=' + BART_API_KEY + '&callback=?', function(xml) {
-    // Parse XML
-    var data = $.xml2json(xml);
-    showingStation = data.station.abbr;
-    activeMarker = marker;
-    showStationInfo(data.station);
-  });
+  $.get(
+    BART_API_URI +
+      "etd.aspx?cmd=etd&orig=" +
+      stationKey +
+      "&key=" +
+      BART_API_KEY +
+      "&callback=?",
+    function(xml) {
+      // Parse XML
+      var data = $.xml2json(xml);
+      showingStation = data.station.abbr;
+      activeMarker = marker;
+      showStationInfo(data.station);
+    }
+  );
 }
 
 function showStationInfo(station) {
@@ -66,19 +81,32 @@ function showStationInfo(station) {
         platforms[plat] = {
           dir: estimate.direction,
           trains: []
-        }
-      };
-      platforms[plat].trains.push({mins: estimate.minutes, destId: destination.abbreviation, dest: destination.destination, color: estimate.color})
+        };
+      }
+      platforms[plat].trains.push({
+        mins: estimate.minutes,
+        destId: destination.abbreviation,
+        dest: destination.destination,
+        color: estimate.color
+      });
     });
   });
-  var stationInfo = 'Station: <b>' + station.name + '</b>';
+  var stationInfo = "Station: <b>" + station.name + "</b>";
   platforms.forEach(function(platform, platId) {
     platform.trains.sort((a, b) => toInt(a.mins) - toInt(b.mins));
-    stationInfo += '<br>Platform ' + platId + ': ' + platform.dir;
+    stationInfo += "<br>Platform " + platId + ": " + platform.dir;
     platform.trains.forEach(function(train) {
-      stationInfo += '<br>' + train.mins + ' min -- ' + debug(train.destId + ': ') + train.dest + ' (' + train.color.toLowerCase() + ')';
+      stationInfo +=
+        "<br>" +
+        train.mins +
+        " min -- " +
+        debug(train.destId + ": ") +
+        train.dest +
+        " (" +
+        train.color.toLowerCase() +
+        ")";
     });
-    stationInfo += '<br>';
+    stationInfo += "<br>";
   });
   activeMarker.bindPopup(stationInfo);
 }
@@ -87,46 +115,59 @@ function showStationInfo(station) {
     Bart Estimated
 \*----------------------------------------------------------------------*/
 function getBART() {
-  $.get(BART_API_URI + 'etd.aspx?cmd=etd&orig=ALL&key=' + BART_API_KEY + '&callback=?', processBART);
+  $.get(
+    BART_API_URI +
+      "etd.aspx?cmd=etd&orig=ALL&key=" +
+      BART_API_KEY +
+      "&callback=?",
+    processBART
+  );
 }
-
+var prevSelection = selectedTrain; //this is for comparing when getting new data
 function processBART(xml) {
   // Parse XML
   var data = $.xml2json(xml);
   refreshCountDown = REFRESH_FREQ;
   // some times we get the same data, or responses out of order, in such case, we just ignore them
-  if (lastProcTime >= data.time) {
+  // a: this if was changed in order to ensure the map is being updated immediately after a filter
+  if (lastProcTime >= data.time && prevSelection === selectedTrain) {
     return;
+  } else if (prevSelection !== selectedTrain) {
+    prevSelection = selectedTrain;
   }
-  lastProcTime = data.time
+  lastProcTime = data.time;
 
-  $('#last_updated').html('Data as of <b>' + data.time + '</b>');
-  var debug = 'Data: ' + data.time;
+  $("#last_updated").html("Data as of <b>" + data.time + "</b>");
+  var debug = "Data: " + data.time;
 
   var trains2 = [];
   debug += computeLiveTrains(data, trains2);
   debug += drawLiveTrains(trains2);
 
-  debugText = debug + '<br><br>' + debugText.substring(0, 6000);
-  $('#debugOutput').html(debugText);
+  debugText = debug + "<br><br>" + debugText.substring(0, 6000);
+  console.log(debugText);
 }
 
 function computeLiveTrains(data, trains) {
-  var debug = '';
+  var debug = "";
   data.station.forEach(function(station) {
     if (showingStation == station.abbr) {
       showStationInfo(station);
     }
     asArray(station.etd).forEach(function(destination) {
       asArray(destination.estimate).forEach(function(estimate) {
-        var route = getRouteInfo(estimate.color, station.abbr, destination.abbreviation);
+        var route = getRouteInfo(
+          estimate.color,
+          station.abbr,
+          destination.abbreviation
+        );
         if (route) {
           var legMins = routeTimes[station.abbr][route.prev];
           if (!legMins) {
             legMins = 0;
           }
           var estimateMins = toInt(estimate.minutes);
-          var destKey = estimate.color + '_' + destination.abbreviation;
+          var destKey = estimate.color + "_" + destination.abbreviation;
           if (!trains[destKey]) {
             trains[destKey] = [];
           }
@@ -150,7 +191,15 @@ function computeLiveTrains(data, trains) {
             };
           }
         } else {
-          debug += '<br>Link NotFound: ' + estimate.color + ', ' + station.abbr + '->' + destination.abbreviation + ',' + estimate.direction;
+          debug +=
+            "<br>Link NotFound: " +
+            estimate.color +
+            ", " +
+            station.abbr +
+            "->" +
+            destination.abbreviation +
+            "," +
+            estimate.direction;
         }
       });
     });
@@ -164,8 +213,23 @@ function computeLiveTrains(data, trains) {
       var next = trains[destIdx][train.nextStation];
       if (prev && prev.etaMins < train.etaMins) {
         train.valid = false;
-      } else if (train.etaMins > 10 && train.prevStation != stationIdx && next && next.etaMins < train.etaMins) {
-        debug += '<br> dir? ' + train.forStation + '/' + train.etaMins + ' next: ' + next.forStation + '/' + next.etaMins + ' -> ' + train.destStation;
+      } else if (
+        train.etaMins > 10 &&
+        train.prevStation != stationIdx &&
+        next &&
+        next.etaMins < train.etaMins
+      ) {
+        debug +=
+          "<br> dir? " +
+          train.forStation +
+          "/" +
+          train.etaMins +
+          " next: " +
+          next.forStation +
+          "/" +
+          next.etaMins +
+          " -> " +
+          train.destStation;
       }
     }
   }
@@ -174,15 +238,36 @@ function computeLiveTrains(data, trains) {
 
 function drawLiveTrains(trains) {
   var renewTrains = [];
-  var debug = '';
+  var tselected = selectedTrain.split(".");
+  var selectedTrainDest = tselected[0];
+  var selectedTrainLine = tselected[1];
+  var debug = "";
   for (var destIdx in trains) {
     for (var stationIdx in trains[destIdx]) {
       var train = trains[destIdx][stationIdx];
-      if (train.valid) {
-        var position = getTrainPosition(train.forStation, train.prevStation, train.etaMins, train.legMins);
+      if (
+        (train.valid && selectedTrain === "all") ||
+        (train.valid &&
+          train.destStation == selectedTrainDest &&
+          train.color == selectedTrainLine)
+      ) {
+        var position = getTrainPosition(
+          train.forStation,
+          train.prevStation,
+          train.etaMins,
+          train.legMins
+        );
         var exisiting = extractPreviousLiveTrain(train, liveTrains);
         if (!exisiting) {
-          var marker = createTrainMarker(train, train.sta, train.etd, train.est, position, train.legMins, train.prevStation)
+          var marker = createTrainMarker(
+            train,
+            train.sta,
+            train.etd,
+            train.est,
+            position,
+            train.legMins,
+            train.prevStation
+          );
           var trainMarker = {
             train: train,
             position: position,
@@ -190,13 +275,27 @@ function drawLiveTrains(trains) {
           };
           map.addLayer(trainMarker.marker);
           renewTrains.push(trainMarker);
-          debug += '<br>(add) ' + getTrainShortInfo(train);
-        } else if (position.lat != exisiting.position.lat || position.lng != exisiting.position.lng) {
+          debug += "<br>(add) " + getTrainShortInfo(train);
+        } else if (
+          position.lat != exisiting.position.lat ||
+          position.lng != exisiting.position.lng
+        ) {
           exisiting.train = train;
           exisiting.adjCount = 30;
-          exisiting.adjLat = (position.lat - exisiting.position.lat) / exisiting.adjCount;
-          exisiting.adjLng = (position.lng - exisiting.position.lng) / exisiting.adjCount;
-          setTrainPopup(exisiting.marker, train, train.sta, train.etd, train.est, position, train.legMins, train.prevStation);
+          exisiting.adjLat =
+            (position.lat - exisiting.position.lat) / exisiting.adjCount;
+          exisiting.adjLng =
+            (position.lng - exisiting.position.lng) / exisiting.adjCount;
+          setTrainPopup(
+            exisiting.marker,
+            train,
+            train.sta,
+            train.etd,
+            train.est,
+            position,
+            train.legMins,
+            train.prevStation
+          );
           renewTrains.push(exisiting);
           // we also want to update the markerText with new/updated info
         } else {
@@ -207,21 +306,34 @@ function drawLiveTrains(trains) {
   }
   liveTrains.forEach(function(trainMarker) {
     map.removeLayer(trainMarker.marker);
-    debug += '<br>(del) ' + getTrainShortInfo(trainMarker.train);
+    debug += "<br>(del) " + getTrainShortInfo(trainMarker.train);
   });
   liveTrains = renewTrains;
   return debug;
 }
 
 function getTrainShortInfo(train) {
-  return train.forStation + ',  ' + train.color + ' -> ' + train.destStation + ': ' + train.etaMins;
+  return (
+    train.forStation +
+    ",  " +
+    train.color +
+    " -> " +
+    train.destStation +
+    ": " +
+    train.etaMins
+  );
 }
 
 // search for train for a possible previous/same position from liveTrains
 function extractPreviousLiveTrain(train, trains) {
   for (var i in trains) {
     var check = trains[i];
-    if (train.destStation == check.train.destStation && train.color == check.train.color && (train.forStation == check.train.forStation || train.prevStation == check.train.forStation)) {
+    if (
+      train.destStation == check.train.destStation &&
+      train.color == check.train.color &&
+      (train.forStation == check.train.forStation ||
+        train.prevStation == check.train.forStation)
+    ) {
       trains.splice(i, 1);
       return check;
     }
@@ -241,21 +353,17 @@ function getRouteInfo(color, curr, dest) {
   }
 
   var dirUp = currIdx < destIdx;
-  var nextIdx = Math.min(Math.max(currIdx + (
-    dirUp
-    ? 1
-    : -1), 0), route.length - 1);
-  var prevIdx = Math.min(Math.max(currIdx - (
-    dirUp
-    ? 1
-    : -1), 0), route.length - 1);
+  var nextIdx = Math.min(
+    Math.max(currIdx + (dirUp ? 1 : -1), 0),
+    route.length - 1
+  );
+  var prevIdx = Math.min(
+    Math.max(currIdx - (dirUp ? 1 : -1), 0),
+    route.length - 1
+  );
   return {
-    icon: dirUp
-      ? routes[color].iconUp
-      : routes[color].iconDown,
-    prev: route[prevIdx]
-      ? route[prevIdx]
-      : '',
+    icon: dirUp ? routes[color].iconUp : routes[color].iconDown,
+    prev: route[prevIdx] ? route[prevIdx] : "",
     next: route[nextIdx]
   };
 }
@@ -265,12 +373,15 @@ function getRouteInfo(color, curr, dest) {
 \*----------------------------------------------------------------------*/
 function setupMap() {
   //create map
-  map = new L.Map('map');
-  var layer = new L.TileLayer('https://mt1.google.com/vt/lyrs=m@121,transit|vm:1&hl=en&opts=r&x={x}&y={y}&z={z}', {
-    attribution: 'Map data &copy;2012 Google',
-    maxZoom: 16
-  });
-  var sf = new L.LatLng(37.735, -122.17);
+  map = new L.Map("map");
+  var layer = new L.TileLayer(
+    "https://mt1.google.com/vt/lyrs=m@121,transit|vm:1&hl=en&opts=r&x={x}&y={y}&z={z}",
+    {
+      attribution: "Map data &copy;2019 Google",
+      maxZoom: 10
+    }
+  );
+  var sf = new L.LatLng(37.735, -122.1);
   map.setView(sf, 11).addLayer(layer);
   drawStations();
 }
@@ -279,17 +390,15 @@ function drawStations() {
   $.each(stations, function(stationKey, station) {
     var marker = new L.Marker(new L.LatLng(station.lat, station.lng), {
       icon: L.divIcon({
-        className: 'station-icon',
-        iconSize: [
-          9, 9
-        ],
+        className: "station-icon",
+        iconSize: [9, 9]
       }),
       title: station.name,
       zIndexOffset: 100,
       opacity: STATION_OPACITY
     });
-    marker.bindPopup('Station: <b>' + station.name + '</b>');
-    marker.on('click', function() {
+    marker.bindPopup("Station: <b>" + station.name + "</b>");
+    marker.on("click", function() {
       getBartStation(stationKey, marker);
     });
     map.addLayer(marker);
@@ -310,15 +419,23 @@ function getTrainPosition(toStation, fromStation, estimateMins, threshold) {
     percent = 0;
   }
 
-  var lat = toStation.lat - ((toStation.lat - fromStation.lat) * percent);
-  var lng = toStation.lng - ((toStation.lng - fromStation.lng) * percent);
+  var lat = toStation.lat - (toStation.lat - fromStation.lat) * percent;
+  var lng = toStation.lng - (toStation.lng - fromStation.lng) * percent;
 
-  return {lat: lat, lng: lng};
+  return { lat: lat, lng: lng };
 }
 
-function createTrainMarker(train, station, destination, estimate, position, threshold, fromStation) {
+function createTrainMarker(
+  train,
+  station,
+  destination,
+  estimate,
+  position,
+  threshold,
+  fromStation
+) {
   var icon = L.divIcon({
-    className: 'train-icon train-' + train.route.icon,
+    className: "train-icon train-" + train.route.icon,
     iconSize: [18, 14]
   });
   var marker = new L.Marker(new L.LatLng(position.lat, position.lng), {
@@ -326,33 +443,70 @@ function createTrainMarker(train, station, destination, estimate, position, thre
     title: train.etd.destination,
     zIndexOffset: 1000
   });
-  setTrainPopup(marker, train, station, destination, estimate, position, threshold, fromStation);
+  setTrainPopup(
+    marker,
+    train,
+    station,
+    destination,
+    estimate,
+    position,
+    threshold,
+    fromStation
+  );
   return marker;
 }
 
-function setTrainPopup(marker, train, station, destination, estimate, position, threshold, fromStation) {
+function setTrainPopup(
+  marker,
+  train,
+  station,
+  destination,
+  estimate,
+  position,
+  threshold,
+  fromStation
+) {
   var iconLabel = stations[destination.abbreviation]
     ? stations[destination.abbreviation].iconAbbreviation
-    : '';
+    : "";
   if (estimate.delay > 0) {
-    iconLabel += '!';
+    iconLabel += "!";
   }
 
   if (marker.options.icon) {
     marker.options.icon.options.html = iconLabel;
   }
 
-  var markerText = '<b>' + destination.destination + '</b> Train';
-  if (estimate.minutes == 'Leaving') {
-    markerText += '<br>Leaving Station: <b>' + stations[station.abbr].name + '</b>';
+  var markerText = "<b>" + destination.destination + "</b> Train";
+  if (estimate.minutes == "Leaving") {
+    markerText +=
+      "<br>Leaving Station: <b>" + stations[station.abbr].name + "</b>";
   } else {
-    markerText += '<br>Next Station: <b>' + stations[station.abbr].name + '</b> in ' + estimate.minutes + ' min' + debug(' (total ' + threshold + ')');
+    markerText +=
+      "<br>Next Station: <b>" +
+      stations[station.abbr].name +
+      "</b> in " +
+      estimate.minutes +
+      " min" +
+      debug(" (total " + threshold + ")");
   }
 
-  markerText += debug('<br> ' + estimate.color + '/' + estimate.direction + ', from: ' + fromStation + ', to: ' + station.abbr + ', final: ' + destination.abbreviation);
+  markerText += debug(
+    "<br> " +
+      estimate.color +
+      "/" +
+      estimate.direction +
+      ", from: " +
+      fromStation +
+      ", to: " +
+      station.abbr +
+      ", final: " +
+      destination.abbreviation
+  );
 
   if (estimate.delay > 0) {
-    markerText += '<br>Delayed: <b>' + secondsToMins(estimate.delay) + '</b> mins.';
+    markerText +=
+      "<br>Delayed: <b>" + secondsToMins(estimate.delay) + "</b> mins.";
   }
 
   marker.bindPopup(markerText);
@@ -373,26 +527,22 @@ function moveTrains() {
     Main
 \*----------------------------------------------------------------------*/
 function debug(text) {
-  return debugMode
-    ? text
-    : '';
+  return debugMode ? text : "";
 }
 
 function sizeWindow() {
-  $('#map').height($(window).height() - 40);
+  $("#map").height($(window).height() - 40);
 }
 
 function developmentMode() {
   debugMode = !debugMode;
-  $('#development')[0].style.visibility = debugMode
-    ? 'visible'
-    : '';
+  $("#development")[0].style.visibility = debugMode ? "visible" : "";
 }
 
 function updateClock() {
   if (refreshCountDown > 0) {
     refreshCountDown -= 1000;
-    $('#clock span').html(refreshCountDown / 1000);
+    $("#clock span").html(refreshCountDown / 1000);
   }
 }
 
